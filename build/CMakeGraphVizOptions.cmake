e408a7ceefd44f6e72c2d0d713cb64bb6ba8f8e9
Adrian Radzikowski <dgtiarov@protonmail.com>
2023-06-03 02:21:24 +0200

    CMake linkage dependency cleanup

--- build/CMakeGraphVizOptions.cmake
+++ build/CMakeGraphVizOptions.cmake
@@ -1,10 +1,10 @@
 # Set graph settings
 SET(GRAPHVIZ_GRAPH_HEADER "\
 rankdir=RL;\
-ranksep=\"0.3\";\
+ranksep=\"1.5\";\
 bgcolor=aliceblue;\
 graph [ nodesep=\"0.5\" ];\
-node [ width=\"1.2\", fontsize=\"15\", color=black, style=filled, fillcolor=azure2 ];\
+node [ height=\"0.9\", width=\"1.2\", fontsize=\"15\", color=black, style=filled, fillcolor=azure2 ];\
 edge [ arrowhead=inv, arrowtail=normal, dir=back, arrowsize=\"1.1\", penwidth=\"2\" color=black ];\
 ")
 
