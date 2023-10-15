a398a94763036b39f2cf92f1b0bba78d69d84d11
loxotron <loxotron@bk.ru>
2023-10-15 10:56:07 +0300

    wpf_controls fix needs improvements

--- build/SetupWPF_Controls.py
+++ build/SetupWPF_Controls.py
@@ -8,3 +8,11 @@ def replaceAll(file,searchExp,replaceExp):
      
     
 replaceAll('X-Ray-v2.0.sln', "xray\wpf_controls\wpf_controls.csproj" , r"..\xray\wpf_controls\wpf_controls.csproj")
+replaceAll('xray\editor\controls\sources\editor_controls.vcxproj', r"xray\build\xray\wpf_controls\wpf_controls.csproj", r"xray\xray\wpf_controls\wpf_controls.csproj")
+replaceAll("xray\editor\\base\sources\editor_base.vcxproj", r"xray\build\xray\wpf_controls\wpf_controls.csproj", r"xray\xray\wpf_controls\wpf_controls.csproj")
+replaceAll('xray\editor\\animation\sources\\animation_editor.vcxproj', r"xray\build\xray\wpf_controls\wpf_controls.csproj", r"xray\xray\wpf_controls\wpf_controls.csproj")
+replaceAll('xray\editor\dialog\sources\dialog_editor.vcxproj', r"xray\build\xray\wpf_controls\wpf_controls.csproj", r"xray\xray\wpf_controls\wpf_controls.csproj")
+replaceAll('xray\editor\model\sources\model_editor.vcxproj', r"xray\build\xray\wpf_controls\wpf_controls.csproj", r"xray\xray\wpf_controls\wpf_controls.csproj")
+replaceAll('xray\editor\particle\sources\particle_editor.vcxproj', r"xray\build\xray\wpf_controls\wpf_controls.csproj", r"xray\xray\wpf_controls\wpf_controls.csproj")
+replaceAll('xray\editor\sound\sources\sound_editor.vcxproj', r"xray\build\xray\wpf_controls\wpf_controls.csproj", r"xray\xray\wpf_controls\wpf_controls.csproj")
+replaceAll('xray\editor\world\sources\editor.vcxproj', r"xray\build\xray\wpf_controls\wpf_controls.csproj", r"xray\xray\wpf_controls\wpf_controls.csproj")
