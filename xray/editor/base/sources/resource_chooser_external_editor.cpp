18f552e9331eb4e91e04f4e23c5d88d310a2557c
loxotron <loxotron@bk.ru>
2023-11-03 08:41:32 +0300

    crash when cancelling look_point anim select you can also clear animation value now

--- xray/editor/base/sources/resource_chooser_external_editor.cpp
+++ xray/editor/base/sources/resource_chooser_external_editor.cpp
@@ -13,10 +13,16 @@ namespace editor_base{
 
 void resource_chooser_external_editor::run_editor ( property^ prop )
 {
-	String^ result;
-	resource_chooser::choose( additional_data, safe_cast<String^>( prop->value ), "", result, true );
+	System::String^ current = nullptr;
+	String^ result = nullptr;
 
-	prop->value = result;
+	if (prop->value)
+		current = safe_cast<System::String^>(prop->value);
+
+	if (resource_chooser::choose(additional_data, current, "", result, true)) 
+	{
+		prop->value = result;
+	}
 }
 
 } // namespace editor_base
